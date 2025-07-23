fb57d203766e8df99cf418805b13650217498bea
limingsong <2276695595@qq.com>
2025-07-23 13:10:21 +0800

    feat: 新增字符串转换

--- any_to_string.py
+++ any_to_string.py
@@ -7,6 +7,7 @@ class AnyToString:
         return {
             "required": {
                 "input": ("ANY", {"default": None, "dynamicPrompts": True}),
+                "input_boolean": ("BOOLEAN", {"default": None, "optional": True}),
             }
         }
 
@@ -14,7 +15,9 @@ class AnyToString:
     FUNCTION = "convert"
     CATEGORY = "utils/convert"
 
-    def convert(self, input):
+    def convert(self, input, as_boolean=None):
+        if as_boolean is not None:
+            return str(as_boolean)
         if input is None:
             return ("",)
         return (str(input),)
